81708d182f22d29222e7f431548b8de0b2b9c93b
teaperr <143550106+teaperr@users.noreply.github.com>
2023-09-30 18:43:36 +0100

    0.1
    Init

--- src/main/java/net/teaper/everything/Everything.java
+++ src/main/java/net/teaper/everything/Everything.java
@@ -3,9 +3,7 @@ package net.teaper.everything;
 import net.fabricmc.api.ModInitializer;
 
 public class Everything implements ModInitializer {
-    /**
-     * Runs the mod initializer.
-     */
+    
     @Override
     public void onInitialize() {
 
