3cdc94dd94693ad2f750b95622f9646f841eaabb
teaperr <143550106+teaperr@users.noreply.github.com>
2023-11-05 01:37:52 +0100

    Added Slop Juice
    the Slop

--- src/main/java/net/teaper/everything/item/EverythingFoodComponents.java
+++ src/main/java/net/teaper/everything/item/EverythingFoodComponents.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.FoodComponent;
+import net.minecraft.item.FoodComponents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
@@ -11,5 +12,7 @@ import net.minecraft.util.registry.Registry;
 import net.teaper.everything.Everything;
 
 public class EverythingFoodComponents {
-    public static final FoodComponent WAFFLE = new FoodComponent.Builder().hunger(7).saturationModifier(0.7f).build();
+    public static final FoodComponent WAFFLE = new FoodComponent.Builder().hunger(7).saturationModifier(0.7f).statusEffect(new StatusEffectInstance(StatusEffects.SATURATION, 100, 1), 0.5f).build();
+    public static final FoodComponent SLOP_JUICE = new FoodComponent.Builder().hunger(0).saturationModifier(0).alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.POISON, 200, 0),1.0f).statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 180, 0), 1.0f).build();
+    public static final FoodComponent SUGAR_BREAD = new FoodComponent.Builder().hunger(0).saturationModifier(0.0f).statusEffect(new StatusEffectInstance(StatusEffects.INSTANT_HEALTH, 1, 0), 1.0f).alwaysEdible().snack().build();
 }
--- src/main/java/net/teaper/everything/item/EverythingItems.java
+++ src/main/java/net/teaper/everything/item/EverythingItems.java
@@ -3,6 +3,9 @@ package net.teaper.everything.item;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.PotionItem;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.Potions;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.teaper.everything.Everything;
@@ -11,6 +14,10 @@ public class EverythingItems {
 
     public static final Item WAFFLE = registerItem("waffle",
             new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(EverythingFoodComponents.WAFFLE)));
+    public static final Item SUGAR_BREAD = registerItem("sugar_bread",
+            new Item (new FabricItemSettings().group(ItemGroup.FOOD).food(EverythingFoodComponents.SUGAR_BREAD)));
+    public static final Item SLOP_JUICE = registerItem("slop_juice",
+            new EverythingSlop(new FabricItemSettings().group(ItemGroup.BREWING).food(EverythingFoodComponents.SLOP_JUICE)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(Everything.MOD_ID, name), item);
